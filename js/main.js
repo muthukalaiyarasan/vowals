b7c32fc07cb954383855c686d4b2b3a837ca6247
muthukalaiyarasan <132567401+muthukalaiyarasan@users.noreply.github.com>
2023-07-27 10:50:51 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let a="fabevy"
+let count=0
+ for(i=0;i<a.length;i++){ 
+   let  b=a[i]
+     if(b=="a" ||b=="u" ||b=="o" ||b=="i" ||b=="e"){ 
+         console.log(b) 
+		 count++ 
+ }
+     }
+ console.log("IN",a,"ther are",count,"viovel letters")
